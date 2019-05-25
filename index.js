efa6fae9f20a6549a754f1afabd97389b6606e48
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-25 17:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
                */
-    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge <= dodgerLeftEdge) {
+    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) {
       return true;
     }
     
