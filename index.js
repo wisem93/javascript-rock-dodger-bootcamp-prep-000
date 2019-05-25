a6cb0c486b978ea6644e74597ce4b68a0d998e55
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-25 17:32:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -172,6 +172,14 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
+   window.requestAnimationFrame(function() {
+     const left = positionToInteger(DODGER.style.left);
+     
+     if (left > 0) {
+       DODGER.style.left = `${left - 4}px`;
+     }
+   });
 }
 
 function moveDodgerRight() {
@@ -180,6 +188,14 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
+   window.requestAnimationFrame(function(){
+     const left = positionToInteger(DODGER.style.left)
+     
+     if (left < 360) {
+       DODGER.style.left = `${left + 4}px`;
+     }
+   });
 }
 
 /**
