14a0dff1030621fb315b250bbd8aa1d6f285cc0b
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-25 17:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,16 +25,17 @@ function checkCollision(rock) {
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
+  
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left);
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = positionToInteger(DODGER.style.right);
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left);
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = positionToInteger(rock.style.right);
+    const rockRightEdge = rockLeftEdge + 20;
 /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
